5962c35aa4864a02459012993a03288ffdfca152
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-04-22 14:37:57 +0000

    added generate-key script to help generate hex encoded crypto random key for use with data encryption and JWT creation.

---
+++ scripts/generate-key.js
@@ -0,0 +1,9 @@
+const crypto = require("node:crypto");
+const fs = require("node:fs");
+
+const bitSize = 256;
+const byteSize = bitSize / 8;
+console.log(`generating crypto random ${byteSize} bytes (${bitSize} bits) and writing as hex to file key.txt`);
+const key = crypto.randomBytes(byteSize).toString("hex");
+fs.writeFileSync("key.txt", key, {encoding: "utf-8"});
+console.log("Done, see: key.txt for the value");
