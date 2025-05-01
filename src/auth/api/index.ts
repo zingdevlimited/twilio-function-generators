bca34bd7dff9212104a6d15c27baa309546bf886
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-05-01 12:53:48 +0000

    updated index for /uth/api to expose all handlers and a combined type.

--- src/auth/api/index.ts
+++ src/auth/api/index.ts
@@ -1,4 +1,9 @@
 import { ApiKeyAuthenticationHandler } from "./apiKey";
 import { FlexTokenAuthenticationHandler } from "./flexToken";
+import { ServiceJwtAuthenticationHandler } from "./jwt";
 
-export type AuthenticationHandler = ApiKeyAuthenticationHandler | FlexTokenAuthenticationHandler;
+export * from "./apiKey";
+export * from "./flexToken";
+export * from "./jwt";
+
+export type AuthenticationHandler = ApiKeyAuthenticationHandler | FlexTokenAuthenticationHandler | ServiceJwtAuthenticationHandler;
