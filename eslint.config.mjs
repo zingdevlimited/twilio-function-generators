c8a10ddef92bc092e888e8222495a87712f3e230
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-05-04 13:19:22 +0000

    formatted eslint config file, and updated to included type-checked so can do type linting rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,54 +10,61 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
 });
 
-export default defineConfig([globalIgnores([
-    "src/auth/web/templates/*.*",
-    "**/.devcontainer",
-    "**/.vscode",
-    "**/.yarn",
-    "**/dist",
-    "**/node_modules",
-    "src/auth/web/templates/compiled.ts",
-    "**/.editorconfig",
-    "**/.eslintignore",
-    "**/.eslintrc",
-    "**/.gitattributes",
-    "**/.gitignore",
-    "**/.prettierignore",
-    "**/.prettierrc",
-    "**/.yarnrc.yml",
-    "**/LICENCE",
-    "**/tsconfig.tsbuildinfo",
-    "**/yarn.lock",
-    "**/*.md",
-    "**/*hbs",
-    "**/.txt",
-    "**/*.json",
-]), {
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+export default defineConfig([
+	globalIgnores([
+		"src/auth/web/templates/*.*",
+		"**/.devcontainer",
+		"**/.vscode",
+		"**/.yarn",
+		"**/dist",
+		"**/node_modules",
+		"src/auth/web/templates/compiled.ts",
+		"**/.editorconfig",
+		"**/.eslintignore",
+		"**/.eslintrc",
+		"**/.gitattributes",
+		"**/.gitignore",
+		"**/.prettierignore",
+		"**/.prettierrc",
+		"**/.yarnrc.yml",
+		"**/LICENCE",
+		"**/tsconfig.tsbuildinfo",
+		"**/yarn.lock",
+		"**/*.md",
+		"**/*hbs",
+		"**/.txt",
+		"**/*.json"
+	]),
+	{
+		extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "prettier"),
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+		plugins: {
+			"@typescript-eslint": typescriptEslint
+		},
 
-    languageOptions: {
-        globals: {
-            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
-        },
+		languageOptions: {
+			globals: {
+				...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"]))
+			},
 
-        parser: tsParser,
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
+			parser: tsParser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: __dirname
+			},
+			ecmaVersion: 12,
+			sourceType: "module"
+		},
 
-    rules: {
-        "@typescript-eslint/no-unused-vars": "error",
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        "@typescript-eslint/switch-exhaustiveness-check": "error",
-    },
-}]);
+		rules: {
+			"@typescript-eslint/no-unused-vars": "error",
+			"@typescript-eslint/consistent-type-definitions": ["error", "type"],
+			"@typescript-eslint/switch-exhaustiveness-check": "error"
+		}
+	}
+]);
