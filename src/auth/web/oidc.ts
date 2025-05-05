d33ff6370bb5de420f9040f4ae172f518fa717db
Dan Hyrjak <dan.hyrjak@zing.dev>
2025-05-05 10:34:25 +0000

    updated OIDC implementation to be more fleshed out and added Twilio Request SID to output to allow for easier tracing if users have issues.
    still a couple of TODO's left in the implementation but enough to now do some initial tests.

--- src/auth/web/oidc.ts
+++ src/auth/web/oidc.ts
@@ -5,10 +5,17 @@ import {
 	ResponseConstructor,
 	TwilioResponse
 } from "@twilio-labs/serverless-runtime-types/types";
+import { Client, ClientAuth } from "oauth4webapi";
 import { URL } from "node:url";
+import { ulid } from "ulidx";
 import z from "zod";
 import { loginRedirectError, loginRedirectSuccess, authServiceCallback } from "./templates/compiled";
-import { getValueFromEvent, isRunningLocally } from "../../helpers";
+import {
+	getQueryStringAndBodyAsUrlSearchParams,
+	getTwilioRequestId,
+	getValueFromEvent,
+	isRunningLocally
+} from "../../helpers";
 
 const OidcConfigSchema = z.object({
 	AUTH_OIDC_SERVER_ISSUER_IDENTIFIER_URL: z.string().url(),
@@ -22,10 +29,15 @@ const OidcConfigSchema = z.object({
 		),
 	AUTH_OIDC_RESPONSE_REDIRECT_TO_FUNCTION_PATH: z.string().min(2).default("/auth/redirect"),
 	AUTH_OIDC_RESPONSE_MODE: z.literal("query").or(z.literal("form_post")).default("query"),
-	AUTH_OIDC_SESSION_MAX_AGE: z.number().int().positive().gte(60).default(3600)
+	AUTH_OIDC_SESSION_MAX_AGE: z.number().int().positive().gte(60).default(3600),
+	AUTH_OIDC_CLIENT_SECRET_AUTH_MODE: z
+		.literal("client_secret_basic")
+		.or(z.literal("client_secret_post"))
+		.default("client_secret_basic")
 });
 
 const AuthStateSchema = z.object({
+	id: z.string().ulid(),
 	codeVerifier: z.string(),
 	expiryEpochMs: z.number(),
 	nonce: z.string()
@@ -34,6 +46,7 @@ type AuthState = z.infer<typeof AuthStateSchema>;
 
 const AuthStateCookieSchema = z.object({
 	authStateEncrypted: z.string(),
+	authTag: z.string(),
 	iv: z.string()
 });
 type AuthStateCookie = z.infer<typeof AuthStateCookieSchema>;
@@ -41,7 +54,6 @@ type AuthStateCookie = z.infer<typeof AuthStateCookieSchema>;
 const ENCRYPTION_CIPHER_ALGORITHM = "aes-256-gcm";
 const LOGIN_TIMEOUT_IN_SECONDS = 1800; //30 minutes
 const AUTH_STATE_COOKIE_ENCODING = "base64";
-const AUTH_TAG_QUERY_STRING_PARAM_ENCODING = "hex";
 
 const getAuthCookieName = (runningOnLocalhost: boolean): string => {
 	return `${runningOnLocalhost ? "" : "__Host_"}z_tfg_oidc_as`;
@@ -73,6 +85,7 @@ const getSessionCookieAttributes = (runningOnLocalhost: boolean, maxAge: number)
 const generateRedirectToAuthServiceSuccessResponse = (
 	Response: ResponseConstructor,
 	runningOnLocalhost: boolean,
+	twilioRequestSid: string,
 	authorizationUrl: URL,
 	authCookieValue: string,
 	cookieSameSite: "Lax" | "None"
@@ -87,7 +100,8 @@ const generateRedirectToAuthServiceSuccessResponse = (
 	res.setCookie(getSessionCookieName(runningOnLocalhost), "", getSessionCookieAttributes(runningOnLocalhost, 0));
 	res.appendHeader("Content-Type", "text/html; charset=utf-8");
 	const body = loginRedirectSuccess({
-		navLinkUrl: authorizationUrl.toString()
+		twilioRequestSid,
+		navLinkUrl: authorizationUrl.href
 	});
 	res.setBody(body);
 	return res;
@@ -96,6 +110,7 @@ const generateRedirectToAuthServiceSuccessResponse = (
 const generateRedirectToAuthServiceErrorResponse = (
 	Response: ResponseConstructor,
 	runningOnLocalhost: boolean,
+	twilioRequestSid: string,
 	message?: string
 ): TwilioResponse => {
 	const res = new Response();
@@ -103,6 +118,7 @@ const generateRedirectToAuthServiceErrorResponse = (
 	res.setCookie(getAuthCookieName(runningOnLocalhost), "", getAuthCookieAttributes(runningOnLocalhost, "Lax", 0));
 	res.appendHeader("Content-Type", "text/html; charset=utf-8");
 	const body = loginRedirectError({
+		twilioRequestSid,
 		message: message ?? "An unexpected error occurred, contact support for assistance."
 	});
 	res.setBody(body);
@@ -112,6 +128,7 @@ const generateRedirectToAuthServiceErrorResponse = (
 const generateAuthServiceCallbackResponse = (
 	Response: ResponseConstructor,
 	runningOnLocalhost: boolean,
+	twilioRequestSid: string,
 	status: "SUCCESS" | "ERROR",
 	message: string,
 	jwt?: string,
@@ -128,6 +145,7 @@ const generateAuthServiceCallbackResponse = (
 	);
 	res.appendHeader("Content-Type", "text/html; charset=utf-8");
 	const body = authServiceCallback({
+		twilioRequestSid,
 		message,
 		status
 	});
@@ -141,15 +159,18 @@ const generateAuthServiceCallbackResponse = (
  * ready to bounce back with the login callback response once the user has authenticated.
  * If an error occurs during generating the redirect URL or auth state, error message is displayed in the HTML page returned.
  * @param context current context of function execution
+ * @param event current event of the function execution
  * @param Response Constructor for creating TwilioResponse object to be returned, pass the global: Twilio.Response property.
  * @returns On success a TwilioResponse object with HTML content to redirect to OIDC provider, and auth state for PKCE in cookies.
  * If error, HTML page with error message and link to home page (/).
  */
 export const generateRedirectToAuthServiceFullPageResponse = async (
 	context: Context<unknown>,
+	event: ServerlessEventObject<unknown>,
 	Response: ResponseConstructor
 ): Promise<TwilioResponse> => {
 	const runningOnLocalhost = isRunningLocally(context);
+	const twilioRequestSid = getTwilioRequestId(event);
 	let onStep:
 		| "ParseConfig"
 		| "CheckResponseMode"
@@ -195,11 +216,12 @@ export const generateRedirectToAuthServiceFullPageResponse = async (
 		const urlScheme = runningOnLocalhost ? "http" : "https";
 		const cookieSameSite = env.AUTH_OIDC_RESPONSE_MODE === "query" ? "Lax" : "None";
 		const path = env.AUTH_OIDC_RESPONSE_REDIRECT_TO_FUNCTION_PATH.trim();
-		const redirectUrl = `${urlScheme}://${context.DOMAIN_NAME}${path.startsWith("/") ? "" : "/"}${path}`;
+		const redirectUrl = `${urlScheme}://${context.DOMAIN_NAME}${path.startsWith("/") ? "" : "/"}${path.endsWith("/") ? path.substring(0, path.length - 1) : path}`;
 		const expiry = new Date();
 		expiry.setSeconds(expiry.getSeconds() + LOGIN_TIMEOUT_IN_SECONDS);
 
 		const authState: AuthState = {
+			id: ulid(),
 			codeVerifier,
 			nonce,
 			expiryEpochMs: expiry.valueOf()
@@ -211,10 +233,11 @@ export const generateRedirectToAuthServiceFullPageResponse = async (
 		const authStateEncrypted =
 			cipher.update(JSON.stringify(authState), "utf8", AUTH_STATE_COOKIE_ENCODING) +
 			cipher.final(AUTH_STATE_COOKIE_ENCODING);
-		const authTag = cipher.getAuthTag().toString(AUTH_TAG_QUERY_STRING_PARAM_ENCODING);
+		const authTag = cipher.getAuthTag().toString(AUTH_STATE_COOKIE_ENCODING);
 
 		const authorizationStateCookie: AuthStateCookie = {
 			authStateEncrypted: authStateEncrypted,
+			authTag,
 			iv: ivBuffer.toString(AUTH_STATE_COOKIE_ENCODING)
 		};
 		const authorizationStateCookieEncodedString = Buffer.from(
@@ -232,47 +255,48 @@ export const generateRedirectToAuthServiceFullPageResponse = async (
 		authorizationUrl.searchParams.set("code_challenge", codeChallenge);
 		authorizationUrl.searchParams.set("code_challenge_method", "S256");
 		authorizationUrl.searchParams.set("nonce", nonce);
-		authorizationUrl.searchParams.set("state", authTag);
+		authorizationUrl.searchParams.set("state", authState.id);
 
 		onStep = "GenerateAuthResponse";
 		return generateRedirectToAuthServiceSuccessResponse(
 			Response,
 			runningOnLocalhost,
+			twilioRequestSid,
 			authorizationUrl,
 			authorizationStateCookieEncodedString,
 			cookieSameSite
 		);
 	} catch (err) {
 		console.error("generateRedirectToAuthService", err);
-		let message: string | undefined;
-		const retryNote = ", please retry or contact support if the issue persists.";
+		let messageDetail: string;
 		switch (onStep) {
 			case "ParseConfig":
-				message = `ERROR: failed to parse OIDC Config from context${retryNote}`;
+				messageDetail = "Failed to parse OIDC Config from context";
 				break;
 			case "CheckResponseMode":
-				message = "ERROR: form_post requested but not supported when running locally.";
+				messageDetail = "FATAL - form_post requested but not supported when running locally";
 				break;
 			case "FetchDiscovery":
-				message = `ERROR: failed to fetch discovery document`;
+				messageDetail = "Failed to fetch OIDC discovery document";
 				break;
 			case "ValidateDiscovery":
-				message = `ERROR: failed to validate OIDC discovery response${retryNote}`;
+				messageDetail = "Failed to validate OIDC discovery document";
 				break;
 			case "AuthorizationEndpointMissing":
-				message = `ERROR: authorization_endpoint missing from discovery response${retryNote}`;
+				messageDetail = "authorization_endpoint missing from discovery document";
 				break;
 			case "GenerateAuthState":
-				message = `ERROR: failed to generate auth state values${retryNote}`;
+				messageDetail = "Failed to generate authState";
 				break;
 			case "GenerateAuthUrl":
-				message = `ERROR: failed to generate auth url${retryNote}`;
+				messageDetail = "Failed to generate auth URL";
 				break;
 			case "GenerateAuthResponse":
-				message = `ERROR: failed to generate response object${retryNote}`;
+				messageDetail = "Failed to generate response object";
 				break;
 		}
-		return generateRedirectToAuthServiceErrorResponse(Response, runningOnLocalhost, message);
+		const message = `ERROR: ${onStep} - ${messageDetail}, please retry or contact support if the issue persists.`;
+		return generateRedirectToAuthServiceErrorResponse(Response, runningOnLocalhost, twilioRequestSid, message);
 	}
 };
 
@@ -281,10 +305,10 @@ export const processAuthServiceCallbackAndReturnFullPageResponse = async (
 	event: ServerlessEventObject<unknown>,
 	Response: ResponseConstructor
 ): Promise<TwilioResponse> => {
+	const twilioRequestSid = getTwilioRequestId(event);
 	const runningOnLocalhost = isRunningLocally(context);
 	let onStep:
 		| "ParseConfig"
-		| "GetStateTag"
 		| "GetCookie"
 		| "ParseAuthCookie"
 		| "DecryptAuthState"
@@ -293,22 +317,17 @@ export const processAuthServiceCallbackAndReturnFullPageResponse = async (
 		| "FetchDiscovery"
 		| "ValidateDiscovery"
 		| "TokenEndpointMissing"
-		| "ValidateCallback" = "ParseConfig";
+		| "SetClientAuth"
+		| "ExtractEventParams"
+		| "GenerateRedirectUrl"
+		| "ValidateAuthResponse"
+		| "AuthTokenRequest"
+		| "ProcessAuthTokenResponse"
+		| "GetValidatedIdTokenClaims"
+		| "UserInfoRequest"
+		| "ProcessUserInfoResponse" = "ParseConfig";
 	try {
 		const env = OidcConfigSchema.parse(context);
-
-		onStep = "GetStateTag";
-		const stateParam = getValueFromEvent(
-			{
-				location: "QueryStringOrBody",
-				parameterName: "state"
-			},
-			event
-		);
-		if (!stateParam) {
-			throw new Error(onStep);
-		}
-		const authTag = Buffer.from(stateParam, AUTH_TAG_QUERY_STRING_PARAM_ENCODING);
 
 		onStep = "GetCookie";
 		const cookieName = getAuthCookieName(runningOnLocalhost);
@@ -327,6 +346,7 @@ export const processAuthServiceCallbackAndReturnFullPageResponse = async (
 		const authCookieJson = Buffer.from(authCookieEncodedString, AUTH_STATE_COOKIE_ENCODING).toString("utf8");
 		const authCookieUnknownObj: unknown = JSON.parse(authCookieJson);
 		const authStateCookie = AuthStateCookieSchema.parse(authCookieUnknownObj);
+		const authTag = Buffer.from(authStateCookie.authTag, AUTH_STATE_COOKIE_ENCODING);
 
 		onStep = "DecryptAuthState";
 		const privateKey = Buffer.from(env.AUTH_OIDC_STATE_ENCRYPTION_KEY_HEX_ENCODED, "hex");
@@ -351,9 +371,18 @@ export const processAuthServiceCallbackAndReturnFullPageResponse = async (
 		}
 
 		onStep = "FetchDiscovery";
-		const { discoveryRequest, processDiscoveryResponse, validateAuthResponse, ClientSecretPost } = await import(
-			"oauth4webapi"
-		);
+		const {
+			discoveryRequest,
+			processDiscoveryResponse,
+			validateAuthResponse,
+			authorizationCodeGrantRequest,
+			processAuthorizationCodeResponse,
+			getValidatedIdTokenClaims,
+			userInfoRequest,
+			processUserInfoResponse,
+			ClientSecretBasic,
+			ClientSecretPost
+		} = await import("oauth4webapi");
 		const issuer = new URL(env.AUTH_OIDC_SERVER_ISSUER_IDENTIFIER_URL);
 		const discoveryRequestResponse = await discoveryRequest(issuer, {
 			algorithm: "oidc"
@@ -365,40 +394,138 @@ export const processAuthServiceCallbackAndReturnFullPageResponse = async (
 			throw new Error(onStep);
 		}
 
-		const clientAuth = oauth.ClientSecretPost(client_secret);
+		onStep = "SetClientAuth";
+		let clientAuth: ClientAuth;
+		switch (env.AUTH_OIDC_CLIENT_SECRET_AUTH_MODE) {
+			case "client_secret_basic":
+				clientAuth = ClientSecretBasic(env.AUTH_OIDC_CLIENT_SECRET);
+				break;
+			case "client_secret_post":
+				clientAuth = ClientSecretPost(env.AUTH_OIDC_CLIENT_SECRET);
+				break;
+			default:
+				throw new Error(onStep);
+		}
 
-		onStep = "ValidateCallback";
+		const client: Client = {
+			client_id: env.AUTH_OIDC_CLIENT_ID
+		};
+
+		onStep = "ExtractEventParams";
+		const eventParams = getQueryStringAndBodyAsUrlSearchParams(event);
+
+		onStep = "GenerateRedirectUrl";
+		const urlScheme = runningOnLocalhost ? "http" : "https";
+		const redirectUrl = `${urlScheme}://${context.DOMAIN_NAME}${context.PATH}`;
+
+		onStep = "ValidateAuthResponse";
 		//nonce, state also
-		validateAuthResponse(
+		const callbackParams = validateAuthResponse(authServer, client, eventParams, authState.id);
+
+		onStep = "AuthTokenRequest";
+		const authCodeResponse = await authorizationCodeGrantRequest(
 			authServer,
-			{
-				client_id: env.AUTH_OIDC_CLIENT_ID
-			},
-			params,
-			skipst
+			client,
+			clientAuth,
+			callbackParams,
+			redirectUrl,
+			authState.codeVerifier
 		);
 
-		// //const clientAuth = ClientSecretPost(env.AUTH_OIDC_CLIENT_SECRET);
-		// //validateAuthResponse(as, client,)
-		// return Promise.resolve(false);
+		onStep = "ProcessAuthTokenResponse";
+		const tokenEndpointResponse = await processAuthorizationCodeResponse(authServer, client, authCodeResponse, {
+			expectedNonce: authState.nonce,
+			requireIdToken: true
+		});
+		console.log("tokenEndpointResponse: ", tokenEndpointResponse);
+
+		onStep = "GetValidatedIdTokenClaims";
+		const idTokenClaims = getValidatedIdTokenClaims(tokenEndpointResponse);
+		console.log("IDTokenClaims: ", idTokenClaims);
+		const sub = idTokenClaims?.sub ?? "";
+
+		//TODO: make optional, might not be required if ID Token is good enough
+		onStep = "UserInfoRequest";
+		const userInfoRequestRawResponse = await userInfoRequest(
+			authServer,
+			client,
+			tokenEndpointResponse.access_token
+		);
+
+		onStep = "ProcessUserInfoResponse";
+		const userInfoResponse = await processUserInfoResponse(authServer, client, sub, userInfoRequestRawResponse);
+		console.log("userInfoResponse", userInfoResponse);
+
+		//TODO: verify claims exist using ZodSchema
+
+		//TODO: generate JWT
+		return generateAuthServiceCallbackResponse(
+			Response,
+			runningOnLocalhost,
+			twilioRequestSid,
+			"SUCCESS",
+			"ALL GOOD IN THE HOOD"
+		);
 	} catch (err) {
 		console.error("processAuthServiceCallbackAndReturnFullPageResponse", err);
-		let message: string | undefined;
-		const retryNote = ", please retry or contact support if the issue persists.";
+		let messageDetail: string;
 		switch (onStep) {
 			case "ParseConfig":
-				message = `ERROR: failed to parse OIDC Config from context${retryNote}`;
+				messageDetail = "Failed to parse OIDC Config from context";
 				break;
 			case "GetCookie":
-				message = `ERROR: failed to read authState cookie${retryNote}`;
+				messageDetail = "Failed to read authState cookie";
 				break;
-			case "GetStateTag":
-				message = `ERROR: state parameter missing from callback${retryNote}`;
+			case "ParseAuthCookie":
+				messageDetail = "Failed to parse authState cookie";
+				break;
+			case "DecryptAuthState":
+				messageDetail = "Failed to decrypt authState cookie";
+				break;
+			case "ParseAuthState":
+				messageDetail = "Failed to parse authState value";
+				break;
+			case "AuthStateExpired":
+				messageDetail = "Login Timeout";
+				break;
+			case "FetchDiscovery":
+				messageDetail = "Failed to fetch OIDC discovery document";
+				break;
+			case "ValidateDiscovery":
+				messageDetail = "Failed to validate OIDC discovery document";
 				break;
 			case "TokenEndpointMissing":
-				message = `ERROR: token_endpoint missing from discovery response${retryNote}`;
+				messageDetail = "token_endpoint missing from discovery document";
+				break;
+			case "SetClientAuth":
+				messageDetail = "Failed to setup OIDC client";
+				break;
+			case "ExtractEventParams":
+				messageDetail = "Failed to extract event parameters";
+				break;
+			case "GenerateRedirectUrl":
+				messageDetail = "Failed to generate redirect URL";
+				break;
+			case "ValidateAuthResponse":
+				messageDetail = "Failed to validate auth response";
+				break;
+			case "AuthTokenRequest":
+				messageDetail = "Failed to fetch access token";
+				break;
+			case "ProcessAuthTokenResponse":
+				messageDetail = "Failed to process access token";
+				break;
+			case "GetValidatedIdTokenClaims":
+				messageDetail = "Failed to validate ID Token claims";
+				break;
+			case "UserInfoRequest":
+				messageDetail = "Failed to fetch user info endpoint";
+				break;
+			case "ProcessUserInfoResponse":
+				messageDetail = "Failed to process user info endpoint";
 				break;
 		}
-		return generateAuthServiceCallbackResponse(Response, runningOnLocalhost, "ERROR", message);
+		const message = `ERROR: ${onStep} - ${messageDetail}, please retry or contact support if the issue persists.`;
+		return generateAuthServiceCallbackResponse(Response, runningOnLocalhost, twilioRequestSid, "ERROR", message);
 	}
 };
